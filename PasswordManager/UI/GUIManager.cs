89eaf4fe40b389f88459b94d3c6ea5afb6a5cf4a
Ben Collins <sstben@gmail.com>
2017-03-11 12:51:06 -0600

    Added help menu

---
+++ PasswordManager/UI/GUIManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using Xwt;
+using Application = Xwt.Application;
+using MenuItem = Xwt.MenuItem;
+using Window = Xwt.Window;
+
+/**
+ * For designing and controlling the visual interface
+ * used to operate the Password Manager.
+ */
+
+namespace PasswordManager.UI
+{
+    class GUIManager
+    {
+
+        [STAThread]
+        static void Main()
+        {
+            Application.Initialize(ToolkitType.Gtk);
+
+            // Initialize main window
+            Window mainWindow = new Window()
+            {
+                Title = "Password Manager",
+                Width = 500,
+                Height = 600
+            };
+
+            Menu mainMenu = new Menu();
+
+            // Adds a 'Help' tab to Main Menu
+            MenuItem helpMenu = new MenuItem("Help");
+            mainMenu.Items.Add(helpMenu);
+
+            mainWindow.MainMenu = mainMenu;
+
+            mainWindow.Show();
+            Application.Run();
+            mainWindow.Dispose();
+        }
+    }
+}
